a4005f01c1524ee9460605c576d20c62875198a1
Ethan Tompkins <ethan.tompkins@parivedasolutions.com>
2025-03-17 21:43:47 -0400

    added lsp configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,6 @@ if not ok_theme then
 end
 
 require('mini.snippets').setup({})
-require('mini.completion').setup({})
 
 require('mini.files').setup({})
 vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<cr>', {desc = 'File explorer'})
@@ -34,44 +33,55 @@ vim.keymap.set('n', '<leader><space>', '<cmd>Pick buffers<cr>', {desc = 'Search 
 vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<cr>', {desc = 'Search all files'})
 vim.keymap.set('n', '<leader>fh', '<cmd>Pick help<cr>', {desc = 'Search help tags'})
 
--- List of compatible language servers is here:
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+-- LSP setup
 require('lspconfig').gopls.setup({})
 require('lspconfig').rust_analyzer.setup({})
+require('lspconfig').pyright.setup({}) 
 
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
   callback = function(event)
     local opts = {buffer = event.buf}
 
-    -- Display documentation of the symbol under the cursor
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-
-    -- Jump to the definition
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-
-    -- Format current file
     vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-
-    -- Displays a function's signature information
     vim.keymap.set('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-
-    -- Jump to declaration
     vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-
-    -- Lists all the implementations for the symbol under the cursor
     vim.keymap.set('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-
-    -- Jumps to the definition of the type symbol
     vim.keymap.set('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-
-    -- Lists all the references
     vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-
-    -- Renames all references to the symbol under the cursor
     vim.keymap.set('n', '<leader>ln', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-
-    -- Selects a code action available at the current cursor position
     vim.keymap.set('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   end,
 })
+
+-- nvim-cmp
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) 
+    end,
+  },
+  mapping = {
+    ['<Tab>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(), 
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' }, 
+    { name = 'luasnip' }, 
+  }, {
+    { name = 'buffer' }, 
+    { name = 'path' },
+  })
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require('lspconfig').pyright.setup({
+  capabilities = capabilities,
+})
+
